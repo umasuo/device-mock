be85e1da6b6cb24d7b5ce8dd39440bef5b2b44e8
davis999 <davis.dai@go6d.com>
2017-09-14 17:42:22 +0800

    init project

---
+++ src/main/java/io/device/mock/Application.java
@@ -0,0 +1,15 @@
+package io.device.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Davis on 17/9/14.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
