b5f77c1aeb63bab8cf882125b87edea0392ef947
davis999 <davis.dai@go6d.com>
2017-09-25 15:13:35 +0800

    refactor code

--- src/main/java/io/device/util/MqttUtil.java
+++ src/main/java/io/device/util/MqttUtil.java
@@ -5,10 +5,14 @@ import io.device.dto.MqttConfig;
 
 import org.apache.commons.lang3.StringUtils;
 import org.fusesource.mqtt.client.BlockingConnection;
+import org.fusesource.mqtt.client.MQTT;
 import org.fusesource.mqtt.client.QoS;
 import org.fusesource.mqtt.client.Topic;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -48,11 +52,44 @@ public class MqttUtil {
   }
 
   public static BlockingConnection connectMqtt(MqttConfig mqttConfig) throws Exception {
-    BlockingConnection con = ConnectionFactory.build(mqttConfig);
+    BlockingConnection con = buildConnection(mqttConfig);
 
     Topic[] topics = {new Topic("device/sub/" + mqttConfig.getUserName(), QoS.AT_LEAST_ONCE)};
     con.subscribe(topics);
     System.out.println("connected to mqtt block");
     return con;
+  }
+
+  private static BlockingConnection buildConnection(MqttConfig mqttConfig) {
+    try {
+      MQTT mqtt = new MQTT();
+      mqtt.setHost(mqttConfig.getHost(), mqttConfig.getPort());
+      mqtt.setUserName(mqttConfig.getUserName());
+      mqtt.setPassword(getPassword(mqttConfig.getPublicKey()));
+
+      BlockingConnection connection = mqtt.blockingConnection();
+      connection.connect();
+
+      return connection;
+    } catch (URISyntaxException e) {
+      throw new RuntimeException("Can not set mqtt host and port");
+    } catch (Exception e) {
+      throw new RuntimeException("Can not connect to mqtt block");
+    }
+  }
+
+  private static String getPassword(String publicKey) {
+    try {
+      byte[] md5 = MessageDigest.getInstance("MD5")
+          .digest(publicKey.getBytes(StandardCharsets.UTF_8));
+      StringBuffer sb = new StringBuffer();
+      for (int i = 0; i < md5.length; i++) {
+        sb.append(Integer.toString((md5[i] & 0xff) + 0x100, 16).substring(1));
+      }
+      return sb.toString().substring(7, 23);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return publicKey;
   }
 }
